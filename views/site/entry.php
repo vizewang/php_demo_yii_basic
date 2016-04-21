0969bcb270306d19c2503836a385a240a38a7735
weizwang <weizwang@ebay.com>
2016-04-21 15:03:08 +0800

    add

--- views/site/entry.php
+++ views/site/entry.php
@@ -10,3 +10,9 @@ use yii\widgets\ActiveForm;
 ?>
 
 <?php $form=ActiveForm::begin();?>
+<?=$form->field($model,'name')?>
+<?=$form->field($model,'email')?>
+<div class="form-group">
+    <?=Html::submitButton('Submit',['class'=>'btn btn-primary'])?>
+</div>
+<?php ActiveForm::end()?>
